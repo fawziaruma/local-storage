ce207d9ccaa3147492fadcf082d698e128bbf639
Fawzia Ruma <fawziaruma0652@gmail.com>
2023-05-13 10:19:52 +0600

    last commit

--- app/script.js
+++ app/script.js
@@ -1,3 +1,4 @@
+// get  input filed valu  clear input  field and return 
 const getInputValueId = id =>{
     const inputField = document.getElementById(id)
     const inputValue = inputField.value 
@@ -9,7 +10,14 @@ const addProduct =()=>{
     const product = getInputValueId('product-name-field')
     const quantity = getInputValueId('product-quantity-field')
     console.log(product, quantity);
+
+    // display product  ui
+    addProductToDisplay(product, quantity)
+
+    //  set to local storage 
     saveItemLocalStorage(product, quantity)
+
+
 }
 
 const getShoppingCartFromLocalStroge = ()=>{
@@ -28,3 +36,28 @@ const saveItemLocalStorage = (product, quantity)=>{
     const cartStringyfi = JSON.stringify(cart)
     localStorage.setItem('cart' , cartStringyfi )
 }
+
+const addProductToDisplay = (product , quantitys) =>{
+
+    const productContainer = document.getElementById('product-container')
+    const li = document.createElement('li')
+    li.innerText =`${product} : ${quantitys}`
+    productContainer.appendChild(li)
+
+}
+
+
+const displayProduct =()=>{
+    const cart = getShoppingCartFromLocalStroge()
+    console.log(cart);
+    for(const product in cart){
+        const quantitys = cart[product]
+        console.log(product, quantitys);
+        addProductToDisplay(product , quantitys)
+    }
+
+
+
+}
+
+displayProduct()
